c2da355a16d5c75bc4b9d72822820803134aa2f1
Seemamaglin <seemamaglinsampath@gmail.com>
2026-04-05 20:21:48 +0530

    Add loadQuiz function

--- Task-5/script.js
+++ Task-5/script.js
@@ -9,10 +9,41 @@ fetch('questions.json')
     .catch(error=>console.error('Error loading quiz data:', error));
 
 const questionEl=document.getElementById('question');
-const answerEls=document.querySelectorAll('.answer');
+const optionEl=document.getElementById('options');
 const submitBtn=document.getElementById('submit');
 const resultEl=document.getElementById('result');
 
-let currentQuestion=0;
+let currentQuestionIndex=0;
 let score=0;
 let selectedAnswer=null;
+
+function loadQuiz(){
+    selectedAnswer=null;
+    optionEl.innerHTML='';
+
+    const currentQuestion=quizData[currentQuestionIndex];
+    questionEl.textContent=currentQuestion.question;
+
+    currentQuestion.options.forEach(option=>{
+        const optionDiv=document.createElement('div');   
+        optionDiv.textContent=option;
+        optionDiv.classList.add('option');
+        
+        //<div class="option">Hyper Text Markup Language</div>
+
+        optionDiv.addEventListener('click',()=>{
+            selectedAnswer=option;
+            optionDiv.classList.add('selected');
+
+        //<div class="option selected">Hyper Text Markup Language</div>
+
+        document.querySelectorAll('.option').forEach(opt=>{
+            opt.classList.remove('selected');
+            //<div class="option">Hyper Text Markup Language</div>
+            });
+
+        optionDiv.classList.add("selected");
+        });
+        optionEl.appendChild(optionDiv);
+    });
+};
