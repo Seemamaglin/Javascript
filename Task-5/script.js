b99ef30db28c933b96ec9694093725d4e6634243
Seemamaglin <seemamaglinsampath@gmail.com>
2026-04-05 19:48:00 +0530

    Connect HTML to js

--- Task-5/script.js
+++ Task-5/script.js
@@ -1,9 +1,18 @@
 let quizData=[];
 
 fetch('questions.json')
-    .then(response => response.json())
+    .then(response => response.json())  //convert json to js object
     .then(data=>{
         quizData=data;
         loadQuiz();
     })
     .catch(error=>console.error('Error loading quiz data:', error));
+
+const questionEl=document.getElementById('question');
+const answerEls=document.querySelectorAll('.answer');
+const submitBtn=document.getElementById('submit');
+const resultEl=document.getElementById('result');
+
+let currentQuestion=0;
+let score=0;
+let selectedAnswer=null;
