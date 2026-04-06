d55ed78759060987e9a558055e5a13273772305e
Seemamaglin <seemamaglinsampath@gmail.com>
2026-04-06 13:36:06 +0530

    Implement scroll event

--- Task-9/script.js
+++ Task-9/script.js
@@ -1,17 +1,17 @@
-
-let page =0;
+let page = 0;
 const content = document.getElementById('content');
 
-async function getInfo(){
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  for (let i=0;i<10;i++){
-    const div=document.createElement('div');
-    div.className='sub-div';
-    div.innerHTML=`Item ${page}${i}`;
+async function getInfo() {
+  await new Promise(resolve => setTimeout(resolve, 500)); 
+  for (let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
+    div.className = 'sub-div';
+    div.innerHTML = `Item ${page * 10 + i + 1}`;
     content.appendChild(div);
   }
   page++;
 }
+
 window.addEventListener("scroll",()=>{
   if (
     document.documentElement.scrollTop+
